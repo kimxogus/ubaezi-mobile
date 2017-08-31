a4555b4a06b65efedb2f02d1fe3aaef4aa7b2f11
kimxogus <kgyoo8232@gmail.com>
2017-08-31 22:01:39 +0900

    no unused imports

--- src/component/InitShell/InitShellContainer.js
+++ src/component/InitShell/InitShellContainer.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import { initApp as initAppAC } from 'redux/action/initApp';
 import { setUser as setUserAC } from 'redux/action/user';
 
-const Container = BaseComponent => {
-  return connect(
+const Container = BaseComponent =>
+  connect(
     ({ init }) => ({ init }),
     dispatch => ({
       initApp: bindActionCreators(initAppAC, dispatch),
       setUser: bindActionCreators(setUserAC, dispatch),
     })
   )(BaseComponent);
-};
 
 export default Container;
