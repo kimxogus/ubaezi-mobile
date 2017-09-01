04014df0e6f4084c7cbb40a3fc9c41b43542da3c
kimxogus <kgyoo8232@gmail.com>
2017-09-01 16:30:39 +0900

    fix progressive

--- src/HOC/progressive.js
+++ src/HOC/progressive.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Platform } from 'react-native';
 import styled from 'styled-components/native';
+
+import { theme } from 'styles/theme';
 
 const Wrapper = styled.View`
   ${({ loading }) => (loading ? `opacity: 0.5` : '')};
 `;
 
-const Progress = Platform.select({
-  android: styled.ProgressBarAndroid.attrs({ styleAttr: 'Large' })``,
-  ios: styled.ProgressViewIOS``,
-});
+const Progress = styled.ActivityIndicator.attrs({
+  animating: true,
+  size: 125,
+  color: theme.bg,
+})`
+
+`;
 
 export default BaseComponent => ({ loading = false, ...otherProps }) => {
   const c = <BaseComponent {...otherProps} />;
