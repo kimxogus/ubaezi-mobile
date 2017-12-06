3fb58c715242d54cae256141b77b404a9323a4f3
kimxogus <kgyoo8232@gmail.com>
2017-12-07 05:50:33 +0900

    no cacheFirst for StoreDetail

--- src/component/Screen/StoreScreen/StoreDetailScreen/StoreDetailScreen.js
+++ src/component/Screen/StoreScreen/StoreDetailScreen/StoreDetailScreen.js
@@ -15,7 +15,7 @@ export default class StoreDetailScreen extends Component {
     const { navigation: { state: { params: { id } = {} } = {} } } = this.props;
     return (
       <Container>
-        <StoreDetail cacheFirst path="stores" id={id} />
+        <StoreDetail path="stores" id={id} />
       </Container>
     );
   }
